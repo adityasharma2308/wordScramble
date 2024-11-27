d8891698ca5aa84b9a2a013b8d6b8874e716f063
Aditya Sharma <adityasharma220803@gmail.com>
2024-11-27 09:36:18 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Aditya Sharma on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
